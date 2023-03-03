73bd5ebf5a66f33e6945df27fa8f27a0503f2ad8
Taufiq270694 <taufiq.hidayat@abcfood.com>
2023-03-03 10:43:30 +0700

    App

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Fragment, } from 'react'
+import { Fragment } from 'react'
 import logo from '../image/Logo (1).png'
 
 import React from "react";
@@ -7,19 +7,19 @@ export default function Navbar({ }) {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
     <>
-      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-gray-100 mb-3">
+      <nav className="sticky top-0  flex flex-wrap items-center justify-between backdrop-blur-sm px-2 py-3 bg-gray-100 mb-2 bg-opacity-80 shadow-lg" >
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
           <img
-                    className="block h-8 w-auto lg:hidden"
-                    src={logo}
-                    alt="Your Company"
-                  />
-                  <img
-                    className="hidden h-8 w-auto hover:scale-125 transition duration-500 lg:block"
-                    src={logo}
-                    alt="Your Company"
-                  />
+              className="block h-8 w-auto lg:hidden"
+              src={logo}
+              alt="Your Company"
+            />
+            <img
+              className="hidden h-8 w-auto hover:scale-125 transition duration-500 lg:block"
+              src={logo}
+              alt="Your Company"
+            />
             <button
               className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
@@ -38,48 +38,48 @@ export default function Navbar({ }) {
             <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
               <li className="nav-item">
                 <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-sky-500 hover:opacity-75"
-                  href="#pablo"
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-sky-500 hover:text-blue-900"
+                  href="#"
                 >
                   <i className="fab fa-facebook-square text-lg leading-lg text-sky-500 opacity-75"></i><span className="ml-2">Service</span>
                 </a>
               </li>
               <li className="nav-item">
                 <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-sky-500 hover:opacity-75"
-                  href="#pablo"
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-sky-500 hover:text-blue-900"
+                  href="#"
                 >
                   <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Product</span>
                 </a>
               </li>
               <li className="nav-item">
                 <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-sky-500 hover:opacity-75"
-                  href="#pablo"
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-sky-500 hover:text-blue-900"
+                  href="#"
                 >
                   <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Our Brands</span>
                 </a>
               </li>
               <li className="nav-item">
                 <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-sky-500 hover:opacity-75"
-                  href="#pablo"
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-sky-500 hover:text-blue-900"
+                  href="#"
                 >
                   <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Plantation</span>
                 </a>
               </li>
               <li className="nav-item">
                 <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-sky-500 hover:opacity-75"
-                  href="#pablo"
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-sky-500 hover:text-blue-900"
+                  href="#"
                 >
                   <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Sustainability</span>
                 </a>
               </li>
               <li className="nav-item">
                 <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-sky-500 hover:opacity-75"
-                  href="#pablo"
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-sky-500 hover:text-blue-900"
+                  href="#"
                 >
                   <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Contact Us</span>
                 </a>
